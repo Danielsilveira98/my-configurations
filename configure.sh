1b2a26897847bd73e9614867286752db5d34566a
Daniel Silveira <danielsilveira98@gmail.com>
2019-06-14 21:22:48 -0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,67 +6,67 @@ echo '---> Downloading yay'
 git clone https://aur.archlinux.org/yay.git
 echo '---> Instaling yay'
 cd yay
-makepkg -si --noconfirm >> $HOME/my-configurations/log/yay.log
+makepkg -si --noconfirm >> $HOME/.my-configurations/log/yay.log
 
 echo '---> Install fonts'
-yay -S community/otf-fira-code --noconfirm >> $HOME/my-configurations/log/otf-fira-code.log
+yay -S community/otf-fira-code --noconfirm >> $HOME/.my-configurations/log/otf-fira-code.log
 
 echo '---> Install Stow'
-yay -S community/stow --noconfirm >> $HOME/my-configurations/log/stow.log
+yay -S community/stow --noconfirm >> $HOME/.my-configurations/log/stow.log
 
 echo '---> Instaling my prograns'
 echo '---> Instaling pacman prograns'
 echo '---> - pywal'
-yay -S community/python-pywal --noconfirm >> $HOME/my-configurations/log/pywal.log
+yay -S community/python-pywal --noconfirm >> $HOME/.my-configurations/log/pywal.log
 echo '---> - firefox'
-yay -S extra/firefox --noconfirm >> $HOME/my-configurations/log/firefox.log
+yay -S extra/firefox --noconfirm >> $HOME/.my-configurations/log/firefox.log
 echo '---> - chromiun'
-yay -S extra/chromium --noconfirm >> $HOME/my-configurations/log/chromium.log
+yay -S extra/chromium --noconfirm >> $HOME/.my-configurations/log/chromium.log
 echo '---> - terminator'
-yay -S community/terminator --noconfirm >> $HOME/my-configurations/log/terminator.log
+yay -S community/terminator --noconfirm >> $HOME/.my-configurations/log/terminator.log
 echo '---> - docker'
-yay -S community/docker --noconfirm >> $HOME/my-configurations/log/docker.log
+yay -S community/docker --noconfirm >> $HOME/.my-configurations/log/docker.log
 echo '---> - docker-compose'
-yay -S community/docker-compose --noconfirm >> $HOME/my-configurations/log/docker-compose.log
+yay -S community/docker-compose --noconfirm >> $HOME/.my-configurations/log/docker-compose.log
 echo '---> - go (golang)'
-yay -S community/go --noconfirm >> $HOME/my-configurations/log/golang.log
+yay -S community/go --noconfirm >> $HOME/.my-configurations/log/golang.log
 echo '---> - vim'
-yay -S extra/vim --noconfirm >> $HOME/my-configurations/log/vim.log
+yay -S extra/vim --noconfirm >> $HOME/.my-configurations/log/vim.log
 echo '---> - fish'
-yay -S community/fish --noconfirm >> $HOME/my-configurations/log/fish.log
+yay -S community/fish --noconfirm >> $HOME/.my-configurations/log/fish.log
 echo '---> - galculator'
 yay -S community/galculator-gtk2 --noconfirm >> $HOME/my-configurations/log/galculator.log
 
 echo '---> Instaling aur prograns'
 echo '---> - ulauncher'
-yay -S aur/ulauncher --noconfirm >> $HOME/my-configurations/log/ulauncher.log
+yay -S aur/ulauncher --noconfirm >> $HOME/.my-configurations/log/ulauncher.log
 echo '---> - gotop'
-yay -S aur/gotop-bin --noconfirm >> $HOME/my-configurations/log/gotop.log
+yay -S aur/gotop-bin --noconfirm >> $HOME/.my-configurations/log/gotop.log
 echo '---> - vscode'
-yay -S aur/visual-studio-code-bin --noconfirm >> $HOME/my-configurations/log/vscode.log
+yay -S aur/visual-studio-code-bin --noconfirm >> $HOME/.my-configurations/log/vscode.log
 echo '---> - spotify'
-yay -S aur/spotify --noconfirm >> $HOME/my-configurations/log/spotify.log
+yay -S aur/spotify --noconfirm >> $HOME/.my-configurations/log/spotify.log
 echo '---> - nvm (node version management)'
-yay -S aur/nvm --noconfirm >> $HOME/my-configurations/log/nvm.log
+yay -S aur/nvm --noconfirm >> $HOME/.my-configurations/log/nvm.log
 
 if [ ! -z "$1" ] && [ $1 = "work" ]; then
   echo '---> Instaling work prograns (pacman)'
   echo '---> - dbeaver'
-  yay -S community/dbeaver --noconfirm >> $HOME/my-configurations/log/dbaver.log
+  yay -S community/dbeaver --noconfirm >> $HOME/.my-configurations/log/dbaver.log
   echo '---> - filezilla'
-  yay -S community/filezilla --noconfirm >> $HOME/my-configurations/log/filezilla.log
+  yay -S community/filezilla --noconfirm >> $HOME/.my-configurations/log/filezilla.log
   echo '---> - s3cmd'
-  yay -S community/s3cmd --noconfirm >> $HOME/my-configurations/log/s3cmd.log
+  yay -S community/s3cmd --noconfirm >> $HOME/.my-configurations/log/s3cmd.log
   echo '---> - google-cloud-sdk'
-  yay -S aur/google-cloud-sdk --noconfirm >> $HOME/my-configurations/log/gcsdk.log
+  yay -S aur/google-cloud-sdk --noconfirm >> $HOME/.my-configurations/log/gcsdk.log
   echo '---> - rvm (ruby version management)'
-  yay -S aur/rvm --noconfirm >> $HOME/my-configurations/log/rvm.log
+  yay -S aur/rvm --noconfirm >> $HOME/.my-configurations/log/rvm.log
   echo '---> - google chrome'
-  yay -S aur/google-chrome --noconfirm >> $HOME/my-configurations/log/chrome.log
+  yay -S aur/google-chrome --noconfirm >> $HOME/.my-configurations/log/chrome.log
 
   echo '---> Instaling work prograns (aur)'
   echo '---> - slack'
-  yay -S aur/slack --noconfirm >> $HOME/my-configurations/log/slack.log
+  yay -S aur/slack --noconfirm >> $HOME/.my-configurations/log/slack.log
 fi
 
 echo '---> Removing original vi'
